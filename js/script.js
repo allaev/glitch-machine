64189fc121b2dc9101a179c206bd8585c5e56e4e
allaev <34710484+allaev@users.noreply.github.com>
2018-03-17 13:02:12 +0500

    decrease the size

--- js/script.js
+++ js/script.js
@@ -1,157 +1,157 @@
 var keyData = {
         q: {
           sound: new Howl({
-            src: ['sounds/brushes.wav']
+            src: ['sounds/brushes.mp3']
           }),
           color: '#1abc9c'
         },
         w: {
           sound: new Howl({
-            src: ['sounds/buzzer.wav']
+            src: ['sounds/buzzer.mp3']
           }),
           color: '#2ecc71'
         },
         e: {
           sound: new Howl({
-            src: ['sounds/chirp.wav']
+            src: ['sounds/chirp.mp3']
           }),
           color: '#3498db'
         },
         r: {
           sound: new Howl({
-            src: ['sounds/clack.wav']
+            src: ['sounds/clack.mp3']
           }),
           color: '#9b59b6'
         },
           t: {
           sound: new Howl({
-            src: ['sounds/coin.wav']
+            src: ['sounds/coin.mp3']
           }),
           color: '#34495e'
         },
         y: {
           sound: new Howl({
-            src: ['sounds/dink.wav']
+            src: ['sounds/dink.mp3']
           }),
           color: '#16a085'
         },
         u: {
           sound: new Howl({
-            src: ['sounds/gong.wav']
+            src: ['sounds/gong.mp3']
           }),
           color: '#27ae60'
         },
         i: {
           sound: new Howl({
-            src: ['sounds/grain.wav']
+            src: ['sounds/grain.mp3']
           }),
           color: '#2980b9'
         },
         o: {
           sound: new Howl({
-            src: ['sounds/gulp.wav']
+            src: ['sounds/gulp.mp3']
           }),
           color: '#8e44ad'
         },
         p: {
           sound: new Howl({
-            src: ['sounds/hatch.wav']
+            src: ['sounds/hatch.mp3']
           }),
           color: '#2c3e50'
         },
         a: {
           sound: new Howl({
-            src: ['sounds/numeral.wav']
+            src: ['sounds/numeral.mp3']
           }),
           color: '#f1c40f'
         },
         s: {
           sound: new Howl({
-            src: ['sounds/ratchet1.wav']
+            src: ['sounds/ratchet1.mp3']
           }),
           color: '#e67e22'
         },
           d: {
           sound: new Howl({
-            src: ['sounds/ratchet2.wav']
+            src: ['sounds/ratchet2.mp3']
           }),
           color: '#e74c3c'
         },
         f: {
           sound: new Howl({
-            src: ['sounds/ring.wav']
+            src: ['sounds/ring.mp3']
           }),
           color: '#95a5a6'
         },
         g: {
           sound: new Howl({
-            src: ['sounds/slap.wav']
+            src: ['sounds/slap.mp3']
           }),
           color: '#f39c12'
         },
         h: {
           sound: new Howl({
-            src: ['sounds/smear.wav']
+            src: ['sounds/smear.mp3']
           }),
           color: '#d35400'
         },
         j: {
           sound: new Howl({
-            src: ['sounds/spank.wav']
+            src: ['sounds/spank.mp3']
           }),
           color: '#1abc9c'
         },
         k: {
           sound: new Howl({
-            src: ['sounds/stress.wav']
+            src: ['sounds/stress.mp3']
           }),
           color: '#2ecc71'
         },
         l: {
           sound: new Howl({
-            src: ['sounds/subratchet.wav']
+            src: ['sounds/subratchet.mp3']
           }),
           color: '#3498db'
         },
         z: {
           sound: new Howl({
-            src: ['sounds/tamb.wav']
+            src: ['sounds/tamb.mp3']
           }),
           color: '#9b59b6'
         },
         x: {
           sound: new Howl({
-            src: ['sounds/trashcan.wav']
+            src: ['sounds/trashcan.mp3']
           }),
           color: '#34495e'
         },
         c: {
           sound: new Howl({
-            src: ['sounds/variation.wav']
+            src: ['sounds/variation.mp3']
           }),
           color: '#16a085'
         },
         v: {
           sound: new Howl({
-            src: ['sounds/vox1.wav']
+            src: ['sounds/vox1.mp3']
           }),
           color: '#27ae60'
         },
         b: {
           sound: new Howl({
-            src: ['sounds/vox2.wav']
+            src: ['sounds/vox2.mp3']
           }),
           color: '#2980b9'
         },
         n: {
           sound: new Howl({
-            src: ['sounds/zing.wav']
+            src: ['sounds/zing.mp3']
           }),
           color: '#8e44ad'
         },
         m: {
           sound: new Howl({
-            src: ['sounds/zip.wav']
+            src: ['sounds/zip.mp3']
           }),
           color: '#2c3e50'
         }
